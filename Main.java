afdc58b84fbece62042e6e60bd39238dd99da5a0
chrisdalpez <84511523+chrisdalpez@users.noreply.github.com>
2021-06-29 23:43:23 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    
+    public static void main(String args[]) {
+
+      System.out.println(sepnum(List.of("Ciao 33", "Hola 12", "Hello 43")));
+        
+    }
+
+    public static HashMap<String, Integer> sepnum(List<String> parole){
+       //con hashmap vanno specificate le variabili che conterrà. 
+
+        HashMap<String, Integer> stringInt = new HashMap<String, Integer>(); 
+        //inizializza l'Hashmap inserendo tipo delle variabili, in questo caso String e Int (numero intero)
+
+        for (String parola : parole) {  //da chiedere
+            String stringPart = parola.split(" ")[0]; //java.split() suddivide una stringa. .split prende il carattere compreso tra "" e taglia la stringa alla prima occorrenza.
+            int intPart = Integer.parseInt(parola.split(" ")[1]);
+            stringInt.put(stringPart, intPart); 
+        }
+
+        for (String parola : stringInt.keySet()) { //fa diventare keyset ogni stringa all'interno della lista, 
+            System.out.println(parola); //ritorna il valore delle stringhe che compongono list.of
+        }
+
+        return stringInt; //ritorna la stringa di cui è stato eseguito l'hashmap 
+    }
+    
+    // TEST
+    
+    
+}
